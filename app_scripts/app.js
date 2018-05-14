4677d67efff22e6eb423086bfcbcb40c98483d99
Atolsada <Atolsadag@gmail.com>
2018-05-14 13:07:40 +0200

    Añadido script en archivo externo

---
+++ app_scripts/app.js
@@ -0,0 +1,4 @@
+/* (Se suelen poner en otro fichero pero si se pone en indes p.e. irá al final del head
+    pero se puede poner al final del body. No es recomendable) */
+        let saludoEs = 'Hola amigo desde fichero js'/* ; - Hay proyectos que piden ponerlos al final de línea en jscript */
+        console.log(saludoEs)
